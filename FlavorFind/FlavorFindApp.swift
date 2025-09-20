ac9682ad63cac711a923d531adcf961f25be687e
Daniel Pratt <daniel@blaumagier.com>
2025-09-20 05:48:34 -0400

    Initial Commit

---
+++ FlavorFind/FlavorFindApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlavorFindApp.swift
+//  FlavorFind
+//
+//  Created by Daniel Pratt on 9/20/25.
+//
+
+import SwiftUI
+
+@main
+struct FlavorFindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlavorFindTests/FlavorFindTests.swift
@@ -0,0 +1,16 @@
+//
+//  FlavorFindTests.swift
+//  FlavorFindTests
+//
+//  Created by Daniel Pratt on 9/20/25.
+//
+
+import Testing
+
+struct FlavorFindTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
